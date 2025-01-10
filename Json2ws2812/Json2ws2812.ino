4137497a65d92525129cd69d13a4a731d5637a16
chinju <160495438+chinjulu@users.noreply.github.com>
2025-01-10 20:44:44 +0800

    Add files via upload
    讓伺服器掛掉的版本

--- Json2ws2812/Json2ws2812.ino
+++ Json2ws2812/Json2ws2812.ino
@@ -10,6 +10,9 @@ const char* password = "wifiyee219";  // wifi密碼
 
 #define switch 17
 #define btnpin 16
+#define CNT 4096
+#define CHUNK_SIZE 100
+
 int ledPins[7] = { 2, 3, 4, 5, 6, 7, 8 };
 CRGB led1[1];
 CRGB led2[1];
@@ -20,7 +23,7 @@ CRGB led6[1];
 CRGB led7[1];
 unsigned long startTime = 0;
 CRGB* leds[7] = { led1, led2, led3, led4, led5, led6, led7 };
-unsigned int array[100][8];
+unsigned int array[CNT][8];
 EasyButton btn1(btnpin, 100, true);
 bool ON = 0;
 bool wifisw = 0;
@@ -44,83 +47,39 @@ void onButton() {
   }
 }
 
-void setup() {
-  //fastled腳位宣告
-  FastLED.addLeds<NEOPIXEL, 2>(led1, 1);
-  FastLED.addLeds<NEOPIXEL, 3>(led2, 1);
-  FastLED.addLeds<NEOPIXEL, 4>(led3, 1);
-  FastLED.addLeds<NEOPIXEL, 5>(led4, 1);
-  FastLED.addLeds<NEOPIXEL, 6>(led5, 1);
-  FastLED.addLeds<NEOPIXEL, 7>(led6, 1);
-  FastLED.addLeds<NEOPIXEL, 8>(led7, 1);
-  pinMode(switch, INPUT_PULLUP);  //wifi mode & memory mode切換腳位
+void fetch(int chunk) {
   HTTPClient http;
-  //淨空
-  FastLED.clear();
-  FastLED.show();
+  String apiurl = "http://140.113.160.136:8000/get_test_lightlist/cnt=";
+  String cnt_str = String(CNT);
+  String mid_str = "/chunk=";
+  String chunk_str = String(chunk);
+  apiurl = apiurl + cnt_str + mid_str + chunk_str;
 
-  delay(5000);
-  Serial.begin(115200);  // 啟動序列埠的連接
-  while (!Serial)
-    continue;
-  //連接wifi
-  wifiConnect();
-  //Serial setting
-  Serial.println("Starting setup...");
-  if (!LittleFS.begin()) {
-    Serial.println("LittleFS initialization failed. Formatting...");
-    if (LittleFS.format()) {
-      Serial.println("Formatting successful! Trying to initialize again...");
-      if (LittleFS.begin()) {
-        Serial.println("LittleFS initialized successfully.");
-      } else {
-        Serial.println("LittleFS initialization failed after formatting.");
-      }
-    } else {
-      Serial.println("Formatting failed!");
-    }
-  } else {
-    Serial.println("LittleFS initialized successfully.");
-  }
-  Serial.println("in setup");
-  //連接api
-  http.begin("http://140.113.160.136:8000/get_test_lightlist/cnt=300");
+  http.begin(apiurl);
   JsonDocument doc;
 
-  //更新光表
-  wifisw = digitalRead(switch);
-  if (wifisw) {
-    Serial.println("Wifi Mode");
-    for (int i = 0; i < 7; i++) {
-      leds[i][0] = CRGB::Red;
-      delay(500);
-    }
-    //處理光表輸入
-    int httpResponseCode = http.GET();
-    if (httpResponseCode > 0) {
-      memory = http.getString();
-      Serial.println("api success");
-      Serial.println("data:");
-      Serial.println(memory);
-    } else {
-      Serial.println("api failed");
-      Serial.printf("HTTP request failed, error code: %d\n", httpResponseCode);
-      Serial.println(http.errorToString(httpResponseCode));  // 詳細的錯誤描述
-    }
-
-    File writefile = LittleFS.open("/lightlist.json", "w");
-    if (writefile) {
-      writefile.println(memory);
-      writefile.close();  // 關閉檔案
-      Serial.println("Success writing");
-    } else {
-      Serial.println("Failed to open file for writing");
-    }
+  int httpResponseCode = http.GET();
+  if (httpResponseCode > 0) {
+    memory = http.getString();
+    Serial.println("api success");
+    Serial.println(apiurl);
+    Serial.print("chunk = ");
+    Serial.println(chunk_str);
+    Serial.println("data:");
+    Serial.println(memory);
   } else {
-    Serial.println("Memory Mode");
-    for (int i = 0; i < 7; i++) {
-      leds[i][0] = CRGB::Green;
-    }
+    Serial.println("api failed");
+    Serial.printf("HTTP request failed, error code: %d\n", httpResponseCode);
+    Serial.println(http.errorToString(httpResponseCode));  // 詳細的錯誤描述
+  }
+
+  File writefile = LittleFS.open("/lightlist.json", "w");
+  if (writefile) {
+    writefile.println(memory);
+    writefile.close();  // 關閉檔案
+    Serial.println("Success writing");
+  } else {
+    Serial.println("Failed to open file for writing");
   }
 
   //開啟記憶體中的光表
@@ -149,22 +108,85 @@ void setup() {
       Serial.println("Failed to create new memoryfile");
     }
   }
+  
+  for (int i = 0; i < min(CHUNK_SIZE, CNT-CHUNK_SIZE*chunk); i++) {
+    array[i+CHUNK_SIZE*chunk][0] = doc["color_data"][i]["time"];
+    array[i+CHUNK_SIZE*chunk][1] = doc["color_data"][i]["head"];
+    array[i+CHUNK_SIZE*chunk][2] = doc["color_data"][i]["shoulder"];
+    array[i+CHUNK_SIZE*chunk][3] = doc["color_data"][i]["chest"];
+    array[i+CHUNK_SIZE*chunk][4] = doc["color_data"][i]["arm_waist"];
+    array[i+CHUNK_SIZE*chunk][5] = doc["color_data"][i]["leg1"];
+    array[i+CHUNK_SIZE*chunk][6] = doc["color_data"][i]["leg2"];
+    array[i+CHUNK_SIZE*chunk][7] = doc["color_data"][i]["shoes"];
+  }
+}
+
+
+void setup() {
+  //fastled腳位宣告
+  FastLED.addLeds<NEOPIXEL, 2>(led1, 1);
+  FastLED.addLeds<NEOPIXEL, 3>(led2, 1);
+  FastLED.addLeds<NEOPIXEL, 4>(led3, 1);
+  FastLED.addLeds<NEOPIXEL, 5>(led4, 1);
+  FastLED.addLeds<NEOPIXEL, 6>(led5, 1);
+  FastLED.addLeds<NEOPIXEL, 7>(led6, 1);
+  FastLED.addLeds<NEOPIXEL, 8>(led7, 1);
+  pinMode(switch, INPUT_PULLUP);  //wifi mode & memory mode切換腳位
+  // HTTPClient http;
+  //淨空
+  FastLED.clear();
+  FastLED.show();
+
+  delay(500);
+  Serial.begin(115200);  // 啟動序列埠的連接
+  while (!Serial)
+    continue;
+  //連接wifi
+  wifiConnect();
+  //Serial setting
+  Serial.println("Starting setup...");
+  if (!LittleFS.begin()) {
+    Serial.println("LittleFS initialization failed. Formatting...");
+    if (LittleFS.format()) {
+      Serial.println("Formatting successful! Trying to initialize again...");
+      if (LittleFS.begin()) {
+        Serial.println("LittleFS initialized successfully.");
+      } else {
+        Serial.println("LittleFS initialization failed after formatting.");
+      }
+    } else {
+      Serial.println("Formatting failed!");
+    }
+  } else {
+    Serial.println("LittleFS initialized successfully.");
+  }
+  Serial.println("in setup");
+
+  // //更新光表
+  wifisw = digitalRead(switch);
+  if (wifisw) {
+    Serial.println("Wifi Mode");
+    for (int i = 0; i < 7; i++) {
+      leds[i][0] = CRGB::Red;
+      delay(500);
+    }
+    //處理光表輸入
+    //連接api
+    for(int k=0; k < double(CNT)/double(CHUNK_SIZE); k++) {
+      fetch(k);
+    }    
+  } else {
+    Serial.println("Memory Mode");
+    for (int i = 0; i < 7; i++) {
+      leds[i][0] = CRGB::Green;
+    }
+  }
 
   FastLED.show();
   delay(500);
   FastLED.clear();
   FastLED.show();
-  //deserializeJson(doc, input);
-  for (int i = 0; i < 100; i++) {
-    array[i][0] = doc["color_data"][i]["time"];
-    array[i][1] = doc["color_data"][i]["head"];
-    array[i][2] = doc["color_data"][i]["shoulder"];
-    array[i][3] = doc["color_data"][i]["chest"];
-    array[i][4] = doc["color_data"][i]["arm_waist"];
-    array[i][5] = doc["color_data"][i]["leg1"];
-    array[i][6] = doc["color_data"][i]["leg2"];
-    array[i][7] = doc["color_data"][i]["shoes"];
-  }
+
   startTime = millis();
   btn1.begin();
   btn1.onPressed(onButton);
@@ -192,7 +214,7 @@ int bright(unsigned int data) {
 void loop() {
   btn1.read();
   if (ON) {
-    if (i < 100 && (millis() - startTime >= array[i][0] * 50)) {
+    if (i < CNT && (millis() - startTime >= array[i][0] * 50)) {
       for (int j = 0; j < 7; j++) {
         leds[j][0] = array[i][j + 1] >> 8;
         leds[j][0].nscale8(bright(array[i][j + 1]));
